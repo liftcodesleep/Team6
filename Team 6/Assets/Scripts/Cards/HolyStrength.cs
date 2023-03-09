25bc4c68ed1c77ca8ed274fd8731e23546075fa9
liftcodesleep <liftcodesleep@gmail.com>
2023-03-08 17:23:14 -0800

    Holy Strength

--- Team 6/Assets/Scripts/Cards/HolyStrength.cs
+++ Team 6/Assets/Scripts/Cards/HolyStrength.cs
@@ -14,10 +14,16 @@ public class HolyStrength : Card
     {
 
         Unit target = hex.Units()[0];
-
-        target.heal(2);
-        target.Strenth += 1;
-
+        if (target.name == "skeleton")
+        {
+            target.HitPoints -= 2;
+            target.Strength -= 1;
+        }
+        else
+        {
+            target.heal(2);
+            target.Strenth += 1;
+        }
 
 
     }
